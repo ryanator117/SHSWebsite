667f5d5c05efae854bcaf013d4f07e7486c9379e
SHSWebDesign <washington_andy+SHSWebDesign@hotmail.com>
2013-10-10 07:44:39 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-	
 
 	// Email cloaking - Prevent bots from sending spam to the site feedback email
 
@@ -13,7 +12,41 @@ $(document).ready(function () {
 	$("#email").mouseout(function() { 
 		// need to change the href attribute of the email_link and
 		// change the innerHTML of the email span to the placeholder
-		$("#email").html("the feedback email address");
+		$("#email").html("email address");
 		$("#email_link").attr("href", "#");
 	});
+
+	// Searchbox opening and closing
+
+	$('#search').click(function (e) {
+		//Takes the id of the clicked element to decide if
+		//the bar should be opened or closed
+		id = e.target.id;
+		if (id == 'OSB') {
+			openSearch();
+		}else if (id == 'CSB') {
+			closeSearch();
+		}else{
+			console.log('Search error');
+		}
+	});
+
 });
+
+var id = null;
+
+function closeSearch() {
+	$('#search').animate({
+		'width' : '32px'
+	});
+	$('#search').html('<a href="#"><img src="images/searchIcon.png" id="OSB" /></a><img id="CSB" src="images/closeSearchButton.png"/>');
+}
+function openSearch() {
+	$('#search').animate({
+		'width' : '256px'
+	}, 400, function () {
+		$('#searchBox').focus();
+	});
+	$('#search').html('<input type="text" id="searchBox" /><img id="CSB" src="images/closeSearchButton.png"/>	');
+	$('#CSB').css('display', 'inline');
+}
