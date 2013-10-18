15b85eedd92a41e391d00a4fa3254e0269e06e65
SHSWebDesign <washington_andy+SHSWebDesign@hotmail.com>
2013-10-18 00:18:56 -0700

    Nav bar diesn't utilize JS asdf

--- js/main.js
+++ js/main.js
@@ -6,18 +6,4 @@ $(document).ready(function () {
 });
 function size() {
 	$('section').css('height', $(window).height() - $('header').height() - $('nav').height() - $('footer').height());
-	
-	$('nav a').css('padding', '5px 0');
-	var width = 0;
-	$('nav a').each(function () {
-		width += $(this).width();
-	});
-	var padding = Math.floor((1000 - width) / 11);
-	$('nav a').css('padding', '5px ' + padding + 'px');
-	width = 0;
-	$('nav a').each(function () {
-		width += $(this).outerWidth();
-	});
-	$('nav').css('width', 1000 - ((1000-width) / 1) + 'px');
-	$('nav').css('padding', '0 ' + ((1000-width) / 2) + 'px');
 }
