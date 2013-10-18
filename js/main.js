cbdfeb5c40b70babdf207ca0549d551fc2e3166c
SHSWebDesign <washington_andy+SHSWebDesign@hotmail.com>
2013-10-17 23:36:23 -0700

    Nav link centering

--- js/main.js
+++ js/main.js
@@ -8,9 +8,16 @@ function size() {
 	$('section').css('height', $(window).height() - $('header').height() - $('nav').height() - $('footer').height());
 	
 	$('nav a').css('padding', '5px 0');
-	var linkLeft = $('nav a').last().offset().left;
-	var containerLeft = $('#container').last().offset().left;
-	var padding = (linkLeft - containerLeft);
-	padding /= 6;
+	var width = 0;
+	$('nav a').each(function () {
+		width += $(this).width();
+	});
+	var padding = Math.floor((1000 - width) / 11);
 	$('nav a').css('padding', '5px ' + padding + 'px');
+	width = 0;
+	$('nav a').each(function () {
+		width += $(this).outerWidth();
+	});
+	$('nav').css('width', 1000 - ((1000-width) / 1) + 'px');
+	$('nav').css('padding', '0 ' + ((1000-width) / 2) + 'px');
 }
