000f672908277c149145c253641f223390e2887e
SHSWebDesign <washington_andy+SHSWebDesign@hotmail.com>
2013-10-18 01:05:03 -0700

    Navigation link stuff

--- js/main.js
+++ js/main.js
@@ -3,7 +3,28 @@ $(document).ready(function () {
 	$(window).resize(function () {
 		size();
 	});
+	
+	$('nav a').click(function () {
+		openLink($(this).attr('href'));
+	});
+	
+	$('#navBG').not('a').click(function () {
+		$(this).fadeOut(400);
+	});
+	
 });
+
 function size() {
 	$('section').css('height', $(window).height() - $('header').height() - $('nav').height() - $('footer').height());
+	var width = 0;
+	$('nav a').each(function () {
+		width += $(this).outerWidth();
+	});
+	$('nav a:first-of-type').css('margin-left', 500 - (width / 2) + 'px');
+}
+
+function openLink(href) {
+	href = href.substring(1);
+	$('#navBG h1').html(href);
+	$('#navBG').fadeIn(400);
 }
