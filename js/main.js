dc02b6265ca246550d5351300f0f174f1ee77dcd
SHSWebDesign <washington_andy+SHSWebDesign@hotmail.com>
2013-10-18 01:22:25 -0700

    More stuff

--- js/main.js
+++ js/main.js
@@ -6,12 +6,7 @@ $(document).ready(function () {
 	
 	$('nav a').click(function () {
 		openLink($(this).attr('href'));
-	});
-	
-	$('#navBG').not('a').click(function () {
-		$(this).fadeOut(400);
-	});
-	
+	});	
 });
 
 function size() {
@@ -21,9 +16,17 @@ function size() {
 		width += $(this).outerWidth();
 	});
 	$('nav a:first-of-type').css('margin-left', 500 - (width / 2) + 'px');
+	
+	$('body').click(function (e) {
+		width = $(window).width() / 2;
+		if (e.pageX < (width - 500) || e.pageX > (width + 500)) {
+			$('#navBG').fadeOut(400);
+		}
+	});
 }
 
 function openLink(href) {
+	$('#navBGnav').html($('nav').html());
 	href = href.substring(1);
 	$('#navBG h1').html(href);
 	$('#navBG').fadeIn(400);
