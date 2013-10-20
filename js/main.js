ea044478350d135cfffb4603638a69532dd9d828
SHSWebDesign <washington_andy+SHSWebDesign@hotmail.com>
2013-10-19 21:15:01 -0700

    Centers links with CSS, not Javascript

--- js/main.js
+++ js/main.js
@@ -7,16 +7,4 @@ $(document).ready(function () {
 
 function size() {
 	$('section').css('height', $(window).height() - $('header').height() - $('nav').outerHeight() - $('footer').outerHeight());
-	var width = 0;
-	$('nav a').each(function () {
-		width += $(this).outerWidth();
-	});
-	$('nav a:first-of-type').css('margin-left', 500 - (width / 2) + 'px');
-	
-	$('body').click(function (e) {
-		width = $(window).width() / 2;
-		if (e.pageX < (width - 500) || e.pageX > (width + 500)) {
-			$('#navBG').fadeOut(400);
-		}
-	});
 }
