88afc35f7a31893f2be53b7ed4e2ee15c77d1585
SHSWebDesign <washington_andy+SHSWebDesign@hotmail.com>
2013-10-19 20:36:29 -0700

    Re coloring the site

--- js/main.js
+++ js/main.js
@@ -3,14 +3,10 @@ $(document).ready(function () {
 	$(window).resize(function () {
 		size();
 	});
-	
-	$('nav a:not(.noLink)').click(function () {
-		openLink($(this).attr('href'));
-	});
 });
 
 function size() {
-	$('section').css('height', $(window).height() - $('header').height() - $('nav').height() - $('footer').height());
+	$('section').css('height', $(window).height() - $('header').height() - $('nav').outerHeight() - $('footer').outerHeight());
 	var width = 0;
 	$('nav a').each(function () {
 		width += $(this).outerWidth();
@@ -22,17 +18,5 @@ function size() {
 		if (e.pageX < (width - 500) || e.pageX > (width + 500)) {
 			$('#navBG').fadeOut(400);
 		}
-	});
-}
-
-function openLink(href) {
-	$('#navBGnav').html($('nav').html());
-	href = href.substring(1);
-	$('#navBG h1').html(href);
-	
-	$('#navBG').fadeIn(400);
-
-	$('nav a:not(.noLink)').click(function () {
-		openLink($(this).attr('href'));
 	});
 }
