230eae379213479b750a5f217c171ef9498e740d
SHSWebDesign <washington_andy+SHSWebDesign@hotmail.com>
2013-10-20 23:01:29 -0700

    asdf

--- js/main.js
+++ js/main.js
@@ -39,11 +39,53 @@ var keys1 = [0];
 var keys = [keys1, keys2, keys3, keys4, keys5, keys6, keys7, keys8, keys9, keys10]
 
 $(document).keydown(function (e) {
+	console.log(e.which);
 	$.each(keys, function (key, value) {
 		value.splice(0, 1);
 		value.push(e.which);
-		console.log(value);
+		test(value)
 	});
 });
+
+function test(array) {
+	if (array.toString() == [38, 38].toString()) {
+		lsd();
+	}
+	if (array.toString() == [88].toString()) {
+		stop();
+	}
+}
+
+(function($)
+{
+    $.fn.removeStyle = function(style)
+    {
+        var search = new RegExp(style + '[^;]+;?', 'g');
+
+        return this.each(function()
+        {
+            $(this).attr('style', function(i, style)
+            {
+                return style.replace(search, '');
+            });
+        });
+    };
+}(jQuery));
+
+var lsdInterval;
+function lsd() {
+	$('*').css('transition', 'background-color .5s');
+	lsdInterval = setInterval(function () {
+		$('*').each(function () {
+			$(this).css('background-color', "#" + (Math.random().toString(16) + '000000').slice(2, 8));
+		});
+	}, 500);
+}
+
+function stop() {
+	clearInterval(lsdInterval);
+	$('*').removeStyle('background-color');
+}
+
 
 
