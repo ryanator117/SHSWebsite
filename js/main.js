f92483beb1f82b4df646885d0339ae7f42061a1a
SHSWebDesign <washington_andy+SHSWebDesign@hotmail.com>
2013-10-20 17:03:00 -0700

    asdf

--- js/main.js
+++ js/main.js
@@ -8,3 +8,11 @@ $(document).ready(function () {
 function size() {
 	$('section').css('height', $(window).height() - $('header').height() - $('nav').outerHeight() - $('footer').outerHeight());
 }
+
+//Calendar
+var currentDate = new Date();
+var currentYear = currentDate.getFullYear();
+var currentMonth = currentDate.getMonth();
+var currentDay = currentDate.getDate();
+
+console.log(currentMonth + "" + currentDay + "" + currentYear);
