7ab37c87d22c8fda3b623349aadccdc99c6b6d18
SHSWebDesign <washington_andy+SHSWebDesign@hotmail.com>
2013-10-20 22:46:14 -0700

    asdf

--- js/main.js
+++ js/main.js
@@ -5,27 +5,6 @@ $(document).ready(function () {
 	});
 });
 
-$(function () {
-	var running = false;
-	$(document).easteregg({
-		sequence : [38, 40],
-		callback : 	function () {
-			var all = $('*');
-			all.css('transition', 'background 0.2s ease-in-out');
-			var interval = window.setInterval(function () {
-				all.each(function () {
-					var letters = '0123456789ABCDEF'.split('');
-					var color = '#';
-					for (var i = 0; i < 6; i++ ) {
-						color += letters[Math.round(Math.random() * 15)];
-					}
-					$(this).css('background-color', color);
-				});
-			}, 250);
-		}
-	});
-});
-
 function size() {
 	$('section').css('height', $(window).height() - $('header').height() - $('nav').outerHeight() - $('footer').outerHeight());
 }
@@ -45,3 +24,26 @@ if (curHour > 12) {
 }else{
 	curPeroid = "AM"
 }
+
+var keys10 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var keys9 = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+var keys8 = [0, 0, 0, 0, 0, 0, 0, 0];
+var keys7 = [0, 0, 0, 0, 0, 0, 0];
+var keys6 = [0, 0, 0, 0, 0, 0];
+var keys5 = [0, 0, 0, 0, 0];
+var keys4 = [0, 0, 0, 0];
+var keys3 = [0, 0, 0];
+var keys2 = [0, 0];
+var keys1 = [0];
+
+var keys = [keys1, keys2, keys3, keys4, keys5, keys6, keys7, keys8, keys9, keys10]
+
+$(document).keydown(function (e) {
+	$.each(keys, function (key, value) {
+		value.splice(0, 1);
+		value.push(e.which);
+		console.log(value);
+	});
+});
+
+
